6a18907653737403bad7e31d85e2c1791560b1bb
Jay Joo <jj534@cornell.edu>
2021-11-21 16:00:23 -0500

    Increase desktop send message modal size

--- client/src/pages/listing/Listing/DesktopListing.jsx
+++ client/src/pages/listing/Listing/DesktopListing.jsx
@@ -1,88 +1,26 @@
-import React from 'react';
-import BmBtn from 'src/components/buttons/BmBtn';
-import Btn from 'src/components/buttons/Btn';
-import InfoBox from 'src/components/displays/InfoBox';
-import Map from 'src/components/displays/Map';
-import PolicyDisclaimer from 'src/components/displays/PolicyDisclaimer';
-import Body from 'src/components/fonts/Body';
-import Heading from 'src/components/fonts/Heading';
-import Subheading from 'src/components/fonts/Subheading';
-import Text from 'src/components/fonts/Text';
-import BackHeader from 'src/components/headers/BackHeader';
-import MainHeader from 'src/components/headers/MainHeader';
-import Input from 'src/components/inputs/Input';
-import { FlexRow } from 'src/components/layouts/Flex';
-import Space from 'src/components/layouts/Space';
-import Modal from 'src/components/views/Modal';
-import HoriCenter from 'src/containers/HoriCenter';
-import formatListingDesc from 'src/util/helpers/formatListingDesc';
-import styled from 'styled-components';
-import IconsSection from './IconsSection';
-import ImgPanels from './ImgPanels';
-import RightSidePanel from './RightSidePanel';
+import React from 'react'
+import BmBtn from 'src/components/buttons/BmBtn'
+import Btn from 'src/components/buttons/Btn'
+import InfoBox from 'src/components/displays/InfoBox'
+import Map from 'src/components/displays/Map'
+import PolicyDisclaimer from 'src/components/displays/PolicyDisclaimer'
+import Body from 'src/components/fonts/Body'
+import Subheading from 'src/components/fonts/Subheading'
+import Text from 'src/components/fonts/Text'
+import BackHeader from 'src/components/headers/BackHeader'
+import MainHeader from 'src/components/headers/MainHeader'
+import Input from 'src/components/inputs/Input'
+import { FlexRow } from 'src/components/layouts/Flex'
+import Space from 'src/components/layouts/Space'
+import Modal from 'src/components/views/Modal'
+import HoriCenter from 'src/containers/HoriCenter'
+import formatListingDesc from 'src/util/helpers/formatListingDesc'
+import styled from 'styled-components'
+import IconsSection from './IconsSection'
+import ImgPanels from './ImgPanels'
+import RightSidePanel from './RightSidePanel'
 
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  
-  @media (min-width: ${(props) => props.theme.md}px) {
-    padding-top: 3rem;
-  }
-`;
-
-const Container = styled.div`
-  width: 100%;
-  max-width: 1200px; 
-`;
-
-const Content = styled.div`
-  margin-top: 1.5rem;
-  
-  @media (min-width: ${(props) => props.theme.md}px) {
-    margin-top: 0;
-    flex: 2;
-    max-width: 700px;
-    padding: 0 1rem;
-  }
-`;
-
-const ListingSection = styled.div`
-  @media (min-width: ${(props) => props.theme.md}px) {
-    padding: 4rem 0 8rem 0;
-  }
-`;
-
-const Section = styled.div`
-  margin: 1rem 0 3.5rem 0;
-  
-  @media (min-width: ${(props) => props.theme.md}px) {
-    margin-top: 0;
-  }
-`;
-
-export const Fullwidth = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const ModalContents = styled.div`
-  text-align: start;
-`;
-
-const RightSidePanelContainer = styled.div`
-  width: 20%;
-  min-width: 300px;
-`;
-
-const ModalButtonContainer = styled(HoriCenter)`
-  margin-top: 1.5rem;
-  margin-bottom: 1rem;
-`;
-
-const DesktopListing = ({ 
+const DesktopListing = ({
   listing,
   searchers,
   signedInUser,
@@ -94,73 +32,77 @@ const DesktopListing = ({
   handleCreateMsg,
 }) => {
   const {
-    imgs, addr, price, user, desc, sold, displayName, cornellOnly,
-    totalRooms, availRooms, bathrooms, type, toCampus,
-    maleRoommates, femaleRoommates, lat, lng,
-  } = listing;
+    imgs,
+    addr,
+    price,
+    user,
+    desc,
+    sold,
+    displayName,
+    cornellOnly,
+    totalRooms,
+    availRooms,
+    bathrooms,
+    type,
+    toCampus,
+    maleRoommates,
+    femaleRoommates,
+    lat,
+    lng,
+  } = listing
 
   return (
     <div>
       <MainHeader />
       <Wrapper>
         <Container>
-          <BackHeader 
-            label='Go back'
-            fullwidth 
-          />
-            {(!listing.active || listing.deleted)
-              ? <Body>This listing is inactive or has been deleted by the owner.</Body>
-              : (
-                <ListingSection>
-                  <ImgPanels imgs={imgs} />
-                  <Space margin='3rem 0' />
-                  <FlexRow justifySpaceBetween>
-                    <Content>
-                      <Section>
-                        <FlexRow justifySpaceBetween alignCenter>
-                          <Text variant='h2'>
-                            {formatListingDesc(listing)}
-                          </Text>
-                          <BmBtn listing={listing} />
-                        </FlexRow>
-                        <Space margin='2rem 0' />
-                        <IconsSection listing={listing} />
-                      </Section>
+          <BackHeader label='Go back' fullwidth />
+          {!listing.active || listing.deleted ? (
+            <Body>This listing is inactive or has been deleted by the owner.</Body>
+          ) : (
+            <ListingSection>
+              <ImgPanels imgs={imgs} />
+              <Space margin='3rem 0' />
+              <FlexRow justifySpaceBetween>
+                <Content>
+                  <Section>
+                    <FlexRow justifySpaceBetween alignCenter>
+                      <Text variant='h2'>{formatListingDesc(listing)}</Text>
+                      <BmBtn listing={listing} />
+                    </FlexRow>
+                    <Space margin='2rem 0' />
+                    <IconsSection listing={listing} />
+                  </Section>
 
-                      {/* sold warning info box */}
-                      {sold && (
-                        <Section>
-                          <InfoBox>
-                            <Text variant='h5'>This listing has been marked as sold</Text>
-                          </InfoBox>
-                        </Section>
-                      )}
+                  {/* sold warning info box */}
+                  {sold && (
+                    <Section>
+                      <InfoBox>
+                        <Text variant='h5'>This listing has been marked as sold</Text>
+                      </InfoBox>
+                    </Section>
+                  )}
 
-                      {/* location section */}
-                      <Section>
-                        <Subheading bold>Location</Subheading>
-                        <Space margin='1.5rem 0' />
-                        {toCampus && (
-                          <Body>{toCampus} km to campus</Body>
-                        )}
-                        <Space margin='1.5rem 0' />
-                        {(lat && lng) && (
-                          <div>
-                            <Map
-                              lat={lat}
-                              lng={lng}
-                            />
-                          </div>
-                        )}
-                      </Section>
+                  {/* location section */}
+                  <Section>
+                    <Subheading bold>Location</Subheading>
+                    <Space margin='1.5rem 0' />
+                    {toCampus && <Body>{toCampus} km to campus</Body>}
+                    <Space margin='1.5rem 0' />
+                    {lat && lng && (
+                      <div>
+                        <Map lat={lat} lng={lng} />
+                      </div>
+                    )}
+                  </Section>
 
-                      {/* description section */}
-                      <Section>
-                        <Subheading bold>Description</Subheading>
-                        <Space margin='1.5rem 0' />
-                        <Body lineHeight={1.5}>{desc}</Body>
-                      </Section>
-                      {/* {availAmenities.length > 0
+                  {/* description section */}
+                  <Section>
+                    <Subheading bold>Description</Subheading>
+                    <Space margin='1.5rem 0' />
+                    <Body lineHeight={1.5}>{desc}</Body>
+                  </Section>
+                  {/* {availAmenities.length > 0
                       && (
                       <Section>
                         <Row><Subheading bold>Amenities</Subheading></Row>
@@ -179,48 +121,90 @@ const DesktopListing = ({
                         </Row>
                       </Section>
                       )} */}
-                    </Content>
-                    <RightSidePanelContainer>
-                      <RightSidePanel 
-                        listing={listing}
-                        handleMsgBtnClick={handleMsgBtnClick}
-                        signedInUser={signedInUser}
-                        searchers={searchers}
-                      />
-                    </RightSidePanelContainer>
-                  </FlexRow>
-                </ListingSection>
-              )}
+                </Content>
+                <RightSidePanelContainer>
+                  <RightSidePanel
+                    listing={listing}
+                    handleMsgBtnClick={handleMsgBtnClick}
+                    signedInUser={signedInUser}
+                    searchers={searchers}
+                  />
+                </RightSidePanelContainer>
+              </FlexRow>
+            </ListingSection>
+          )}
         </Container>
       </Wrapper>
-      <Modal
-        open={open}
-        handleClose={handleClose}
-        heading={`Message ${user.name.split(' ')[0]}`}
-      >
+      <Modal open={open} handleClose={handleClose} heading={`Message ${user.name.split(' ')[0]}`}>
         <ModalContents>
-          <Input
-            multiline
-            rows={5}
-            value={msg}
-            onChange={(e) => setMsg(e.target.value)}
-          />
+          <Input multiline rows={10} value={msg} onChange={(e) => setMsg(e.target.value)} />
           <ModalButtonContainer>
-            <Btn
-              color="primary"
-              inverted
-              onClick={handleCreateMsg}
-            >
+            <Btn color='primary' inverted onClick={handleCreateMsg}>
               Send Message
             </Btn>
           </ModalButtonContainer>
-          <PolicyDisclaimer
-            action='sending a message on Cornlet'
-          />
+          <PolicyDisclaimer action='sending a message on Cornlet' />
         </ModalContents>
       </Modal>
     </div>
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media (min-width: ${(props) => props.theme.md}px) {
+    padding-top: 3rem;
+  }
+`
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
+`
+
+const Content = styled.div`
+  margin-top: 1.5rem;
+
+  @media (min-width: ${(props) => props.theme.md}px) {
+    margin-top: 0;
+    flex: 2;
+    max-width: 700px;
+    padding: 0 1rem;
+  }
+`
+
+const ListingSection = styled.div`
+  @media (min-width: ${(props) => props.theme.md}px) {
+    padding: 4rem 0 8rem 0;
+  }
+`
+
+const Section = styled.div`
+  margin: 1rem 0 3.5rem 0;
+
+  @media (min-width: ${(props) => props.theme.md}px) {
+    margin-top: 0;
+  }
+`
+
+const ModalContents = styled.div`
+  text-align: start;
+
+  @media (min-width: ${(props) => props.theme.md}px) {
+    width: 50vw;
+  }
+`
+
+const RightSidePanelContainer = styled.div`
+  width: 20%;
+  min-width: 300px;
+`
+
+const ModalButtonContainer = styled(HoriCenter)`
+  margin-top: 1.5rem;
+  margin-bottom: 1rem;
+`
 
 export default DesktopListing
