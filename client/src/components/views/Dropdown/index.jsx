cb9187c9d21ca34aceca9719781c1a4d6f559b30
Jay Joo <jj534@cornell.edu>
2021-11-25 12:01:50 -0500

    Prevent outside click propagation with overlay

--- client/src/components/views/Dropdown/index.jsx
+++ client/src/components/views/Dropdown/index.jsx
@@ -1,25 +1,66 @@
-import React, { useEffect, useRef } from 'react';
-import styled from 'styled-components';
-import useOnOutsideClick from 'src/util/hooks/useOnOutsideClick';
+import React, { useRef } from 'react'
+import styled from 'styled-components'
+
+const Dropdown = ({ show, setShow, children, alignLeft, alignRight, alignTop, ...rest }) => {
+  const handleClick = (e) => {
+    e.stopPropagation()
+  }
+  const DropdownRef = useRef()
+
+  const closeDropdown = () => {
+    setShow(false)
+  }
+
+  const handleOverlayClick = (event) => {
+    event.stopPropagation()
+    event.preventDefault()
+    closeDropdown()
+  }
+
+  return (
+    <>
+      <Container
+        ref={DropdownRef}
+        onClick={handleClick}
+        show={show}
+        alignLeft={alignLeft}
+        alignRight={alignRight}
+        alignTop={alignTop}
+        {...rest}>
+        {children}
+      </Container>
+      {show && <Overlay onClick={handleOverlayClick} />}
+    </>
+  )
+}
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 998;
+`
 
 const Container = styled.div`
   position: absolute;
   margin-top: 10px;
   left: 0;
   right: 0;
-  z-index: 5;
+  z-index: 999;
 
   @media (min-width: ${(props) => props.theme.md}px) {
     top: 40px;
     width: auto;
 
     // alignLeft
-    left: ${(props) => (props.alignLeft && 'initial')};
+    left: ${(props) => props.alignLeft && 'initial'};
     right: initial;
 
     // alignRight
-    left: ${(props) => (props.alignRight && '-375px')};
-    right: ${props => props.alignRight && '0'};
+    left: ${(props) => props.alignRight && '-375px'};
+    right: ${(props) => props.alignRight && '0'};
 
     // alignTop
     top: ${(props) => (props.alignTop ? '0' : '')};
@@ -35,33 +76,6 @@ const Container = styled.div`
 
   // alignTop
   margin-top: ${(props) => (props.alignTop ? '0' : '')};
-`;
+`
 
-const Dropdown = ({
-  show, setShow, children, alignLeft, alignRight, alignTop, ...rest
-}) => {
-  const handleClick = (e) => {
-    e.stopPropagation();
-  };
-  const DropdownRef = useRef();
-  const closeDropdown = () => {
-    setShow(false);
-  };
-  useOnOutsideClick(DropdownRef, closeDropdown);
-
-  return (
-    <Container
-      ref={DropdownRef}
-      onClick={handleClick}
-      show={show}
-      alignLeft={alignLeft}
-      alignRight={alignRight}
-      alignTop={alignTop}
-      {...rest}
-    >
-      {children}
-    </Container>
-  );
-};
-
-export default Dropdown;
+export default Dropdown
