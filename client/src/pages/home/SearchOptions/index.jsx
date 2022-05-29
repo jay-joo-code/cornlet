09b66d4daa5120fd4b104b48f7e81e45e0a20835
Jay Joo <jj534@cornell.edu>
2022-05-29 09:38:03 -0700

    Reduce filter button bottom margin

--- client/src/pages/home/SearchOptions/index.jsx
+++ client/src/pages/home/SearchOptions/index.jsx
@@ -16,7 +16,7 @@ const SearchOptions = () => {
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  margin: 2rem 0;
+  margin: 2rem 0 1rem 0;
 `
 
 const Container = styled.div`
