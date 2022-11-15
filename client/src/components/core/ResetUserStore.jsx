1d994fff1c7c5b0bfc853c6b7e5be317f808edb2
Jay Joo <jj534@cornell.edu>
2022-11-15 15:00:02 -0500

    reset user store

--- client/src/components/core/ResetUserStore.jsx
+++ client/src/components/core/ResetUserStore.jsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
-import { useSelector, useDispatch } from 'react-redux';
-
-const Container = styled.div`
-
-`;
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import { useSelector, useDispatch } from 'react-redux'
 
 const ResetUserStore = () => {
-  const user = useSelector(state => state.user);
-  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user)
+  const dispatch = useDispatch()
 
   if (user && (user.displayName || !user.name)) {
     dispatch({
@@ -16,25 +12,6 @@ const ResetUserStore = () => {
       payload: null,
     })
   }
-
-  // user env emulation in dev
-  useEffect(() => {
-    // if (process.env.NODE_ENV === 'development') {
-    //   const emulationData = {
-    //     bm: { listings: [], notif: false },
-    //     chatrooms: [],
-    //     _id: '5f07c79147f8400017c27652',
-    //     uid: '110200881369188042103',
-    //     name: 'Chanchal Pramanik',
-    //     photo: 'https://lh3.googleusercontent.com/a-/AOh14GjIPuBCNUcEBuRjHuftmG5gOV-x7AWCLaKipN7MuQ',
-    //     email: 'cpramanik@gmail.com',
-    //   }
-    //   dispatch({
-    //     type: 'USER_SET',
-    //     payload: emulationData,
-    //   })
-    // }
-  }, [])
 
   // create test scenario: flawed user schema saved to redux store
   // useEffect(() => {
@@ -47,7 +24,7 @@ const ResetUserStore = () => {
   //   })
   // }, [])
 
-  return <div />;
-};
+  return <div />
+}
 
-export default ResetUserStore;
+export default ResetUserStore
