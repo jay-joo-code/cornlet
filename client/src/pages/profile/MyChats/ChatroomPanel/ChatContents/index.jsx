281aafc2b6dfadcef2dd5c0213207e6342f57d4e
Jay Joo <jj534@cornell.edu>
2022-11-15 14:58:44 -0500

    hide messages from banned accounts

--- client/src/pages/profile/MyChats/ChatroomPanel/ChatContents/index.jsx
+++ client/src/pages/profile/MyChats/ChatroomPanel/ChatContents/index.jsx
@@ -1,62 +1,59 @@
-import React, { useRef, useLayoutEffect } from 'react';
-import Space from 'src/components/layouts/Space';
-import styled from 'styled-components';
-import MsgGroup from './MsgGroup';
+import React, { useRef, useLayoutEffect } from 'react'
+import Space from 'src/components/layouts/Space'
+import styled from 'styled-components'
+import MsgGroup from './MsgGroup'
 
 const Container = styled.div`
   max-height: 100%;
   padding: 1rem;
-`;
+`
 
 const ChatContents = ({ chatroom }) => {
-  const msgGroups = [];
+  const msgGroups = []
 
   if (chatroom.msgs) {
     chatroom.msgs.forEach((msg) => {
-      const isOwner = msg.uid === chatroom.listing.user.uid;
+      const isOwner = msg.uid === chatroom.listing.user.uid
 
       if (msgGroups.length > 0 && msgGroups[msgGroups.length - 1].isOwner === isOwner) {
         // msg by same user as previous msgGroup
         // merge msg
-        msgGroups[msgGroups.length - 1].msgs.push(msg);
-      }
-      else {
+        msgGroups[msgGroups.length - 1].msgs.push(msg)
+      } else {
         // msg by different user
         // append new msgGroup
-        const user = isOwner 
-          ? chatroom.listing.user 
-          : chatroom.searcher || chatroom.listing.user
+        const user = isOwner ? chatroom.listing.user : chatroom.searcher || chatroom.listing.user
 
         const data = {
           user,
           msgs: [msg],
           isOwner,
-        };
-        msgGroups.push(data);
+        }
+        msgGroups.push(data)
       }
-    });
+    })
   }
 
-  const messagesEndRef = useRef(null);
+  const messagesEndRef = useRef(null)
   const scrollToBottom = () => {
-    messagesEndRef.current.scrollIntoView({ block: 'nearest' });
-  };
+    messagesEndRef.current.scrollIntoView({ block: 'nearest' })
+  }
 
   useLayoutEffect(() => {
-    scrollToBottom();
-  }, [msgGroups]);
+    scrollToBottom()
+  }, [msgGroups])
+
+  if (chatroom.searcher && chatroom.searcher.isBanned) return <div ref={messagesEndRef} />
 
   return (
     <Container>
       {msgGroups.map((msgGroup) => (
-        <MsgGroup
-          {...msgGroup}
-        />
+        <MsgGroup {...msgGroup} />
       ))}
       <Space padding='.5rem 0' />
       <div ref={messagesEndRef} />
     </Container>
-  );
-};
+  )
+}
 
-export default ChatContents;
+export default ChatContents
