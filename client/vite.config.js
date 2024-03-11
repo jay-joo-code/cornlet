78a37b1ea4c08144be7c6dccc0dc4a4b342061e7
Jay Joo <jj534@cornell.edu>
2024-03-11 19:56:03 -0400

    fix svgr configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [svgr({ include: '**/*.svg' }), react()],
   resolve: {
     alias: {
       src: "/src",
